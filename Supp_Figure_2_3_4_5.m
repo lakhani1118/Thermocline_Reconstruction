4f92ef66aa77d69092a4f5779f3e831b47fc8533
Karim Lakhani <k_lakhani@bellsouth.net>
2024-03-09 20:53:04 -0600

    Update Supp_Figure_2_3_4_5.m

--- Supp_Figure_2_3_4_5.m
+++ Supp_Figure_2_3_4_5.m
@@ -3,11 +3,11 @@ addpath('Functions')
 addpath('Data')
 load('Z20.mat')
 load('Depth_Distributions.mat')
-a=find(good_data(:,1)==-7.133);
-b=find(good_data(:,1)==0.83);
-c=find(good_data(:,1)==0.1830);
+% a=find(good_data(:,1)==-7.133);
+% b=find(good_data(:,1)==0.83);
+% c=find(good_data(:,1)==0.1830);
 good_data(:,4)=good_data(:,4)*nan; %Remove T. sacculifer from code
-MC_num=1;
+MC_num=100;
 
 load('d18O_paper.mat')
 Lat=squeeze(X3(:,:,1));
@@ -17,32 +17,25 @@ depthvec=squeeze(Z3(1,1,:));
 latvec=cycleLat(1,:);
 latvec=latvec(:);
 lonvec=cycleLon(:,1);
-s=size(sample);
 true_climatology=[];
-for i=1:3
-    [~,I1]=min(abs(lonvec-sample(i,2)));
-    [~,I2]=min(abs(latvec-sample(i,1)));
-    true_climatology(i,:)=squeeze(cycled18O(I1,I2,:));
-    %Get predicted climatology that the model is compared to
-end
 ACD=[18,32,210,114,96];
 benthic_d18O=1.75;
 
 s2=size(good_data);
 goods=find(~any(isnan(good_data(:,[3,5,6,7])),2));
 good_data2=good_data(goods,:);
-good_data2(good_data2(:,1)==good_data(a,1),:)=[];
-good_data2(good_data2(:,1)==good_data(b,1),:)=[];
-good_data2(good_data2(:,1)==good_data(c,1),:)=[];
+% good_data2(good_data2(:,1)==good_data(a,1),:)=[];
+% good_data2(good_data2(:,1)==good_data(b,1),:)=[];
+% good_data2(good_data2(:,1)==good_data(c,1),:)=[];
 s2=size(good_data2);
 
 groups=[1,5; %WPWP cores
-                6,8; %WP cores
-                11,14; %CP cores
-                15,19]; %EP cores
+                6,9; %WP cores
+                12,16; %CP cores
+                17,22]; %EP cores
 s2=size(groups);
-
-for h=1:s2(1)
+%%
+for h=2:s2(1)
     for i=groups(h,1):groups(h,2)
         depth_plot=linspace(0,610,1000);
         [~,I1]=min(abs(lonvec-good_data2(i,2)));
@@ -60,7 +53,8 @@ for h=1:s2(1)
         else
             
         end
-        true_climatology=squeeze(cycled18O(I1,I2,:));   
+        true_climatology=squeeze(cycled18O(I1,I2,:));
+        true_climatology=true_climatology+fliplr(1:length(true_climatology))*0.00000001;
     
     
         figure('Position',[731.4,317.8,529.6,445.2])
@@ -89,11 +83,16 @@ for h=1:s2(1)
             [MC_beta,MC_MLD]=run_thermocline_model3(MC_depths,[good_data2(i,3:7),benthic_d18O]);
             MC_with(depth_plot<MC_MLD)=good_data2(i,3);
             MC_with(depth_plot>=MC_MLD)=log_func(depth_plot(depth_plot>=MC_MLD),MC_beta);
-            plot(squeeze(MC_with(:)),depth_plot,'-','LineWidth',1,'Color',[0,0,1,5/(MC_num)])
+            plot(squeeze(MC_with(:)),depth_plot,'-','LineWidth',1,'Color',[0,0,1,5/MC_num]);
         end
-        title(strcat("Lat: ",num2str(good_data2(i,1))," ; Lon: ",num2str(good_data2(i,2))));
+        
+        n=find(isnan(true_climatology),5,'first')-1;
+        Z20_equiv=interp1(true_climatology(1:n),depthvec(1:n),-0.66);
+        plot([-0.66,-0.66],[Z20_equiv-100,Z20_equiv+100],'--','Color',0.15*[1,1,1],'LineWidth',1)
+        
+        %title(strcat("Lat: ",num2str(good_data2(i,1))," ; Lon: ",num2str(good_data2(i,2))));
         xlabel('\delta^{18}O_c (‰)')
         ylabel('Depth (m)')
-        pause()
     end
+    pause()
 end
