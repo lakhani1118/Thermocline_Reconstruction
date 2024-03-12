739ec6702499b7988dc5988e72bbcbb7d6e6b13c
Karim Lakhani <k_lakhani@bellsouth.net>
2024-03-11 21:52:49 -0500

    Create Table_2.m

---
+++ Table_2.m
@@ -0,0 +1,2 @@
+%Run Figure_4 code before creating this table
+[good_data(:,1:2),round(Z20'),round(mean(Z20_MC,2)),round(std(Z20_MC,'',2)),round(prctile(Z20_MC,2.5,2)),round(prctile(Z20_MC,97.5,2))]
